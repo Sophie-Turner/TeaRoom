9d70ccb6d2f87ad0d028a83053999d114f60cf91
Squidgeypea <sophie.turner@students.plymouth.ac.uk>
2019-12-11 16:49:37 +0000

    Improved structure of cafe menu.

--- TeaRoomCw/TeaRoomCw/Controllers/CustomerController.cs
+++ TeaRoomCw/TeaRoomCw/Controllers/CustomerController.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TeaRoomCw.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace TeaRoomCw.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly TheContext theContext;
+
+        public CustomerController(TheContext context)
+        {
+            theContext = context;
+        }
         public IActionResult Index()
         {
+            IEnumerable<> menuItems = (from eachItem in theContext.Items.AsEnumerable()
+                                                     select new SelectListItem
+                                                     {
+                                                         Text = eachItem.itemName,
+                                                         Value = eachItem.itemId.ToString()
+                                                     }).ToList();
+
+            ViewBag.Items = menuItems;
             return View();
         }
+
+       
     }
 }
